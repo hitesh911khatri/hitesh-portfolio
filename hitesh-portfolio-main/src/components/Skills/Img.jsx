eda1b230d024acaf779dce62d22a83c6089226a0
hitesh911khatri <149157855+hitesh911khatri@users.noreply.github.com>
2024-07-19 02:29:54 +0530

    Add files via upload

---
+++ hitesh-portfolio-main/src/components/Skills/Img.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+
+
+export default [
+    { id:"1", img:"./SkillsImg/1.png"} ,
+    { id:"2", img:"./SkillsImg/2.png"}  ,
+    { id:"3", img:"./SkillsImg/3.png"}  ,
+    { id:"4", img:"./SkillsImg/4.png"}  ,
+    { id:"5", img:"./SkillsImg/5.png"}  ,
+    { id:"6", img:"./SkillsImg/6.png"}  ,
+    { id:"7", img:"./SkillsImg/7.png"}  ,
+    { id:"8", img:"./SkillsImg/8.png"}  ,
+    { id:"9", img:"./SkillsImg/9.png"}  ,
+    { id:"10", img:"./SkillsImg/10.png"} , 
+    { id:"11", img:"./SkillsImg/11.png"} , 
+ ]
